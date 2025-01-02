9e611d18831da0e188bd01d12bba3d5ee7511137
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-01-03 01:48:46 +0700

    Transformed postcss config js to ts.

--- src/postcss/plugin.ts
+++ src/postcss/plugin.ts
@@ -1,15 +1,15 @@
 let postcss = require('postcss');
 
-const getDeclarationsForSelector = (selector, css) => {
-    const decls = [];
+const getDeclarationsForSelector = (selector: string, css: { walkRules: (arg0: (rule: any) => void) => void; }) => {
+    const decls: any[] = [];
 
     css.walkRules(rule => {
-        let ruleHasSelector = rule.selectors.some(ruleSelector => {
+        let ruleHasSelector = rule.selectors.some((ruleSelector: string) => {
             return ruleSelector === selector;
         });
 
         if (ruleHasSelector) {
-            rule.walkDecls(decl => {
+            rule.walkDecls((decl: any) => {
                 decls.push(decl);
             });
         }
@@ -21,11 +21,11 @@ const getDeclarationsForSelector = (selector, css) => {
 module.exports = (opts = {}) => {
     return {
         postcssPlugin: '${pluginName}',
-        Once(root) {
+        Once(root: { walkAtRules: (arg0: string, arg1: (rule: any) => void) => void; source: any; }) {
             root.walkAtRules('apply', (rule) => {
                 const classes = rule.params.toString().replace(/\s/g, " ").split(" ");
 
-                classes.forEach(selector => {
+                classes.forEach((selector: any) => {
                     const decls = getDeclarationsForSelector(`.${selector}`, rule.root());
 
                     decls.forEach(decl => {
