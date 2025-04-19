aa7ca6eb4f2d5f5d2153ea78f3395d7999b09ba2
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-04-19 12:35:55 +0700

    Added styles of break before (layout).

--- src/styles/layout.ts
+++ src/styles/layout.ts
@@ -1,9 +1,11 @@
 import columns from './layout/columns';
 import breakAfter from './layout/breakAfter';
+import breakBefore from './layout/breakBefore';
 import aspectRatio from './layout/aspectRatio';
 
 export default {
     columns,
     breakAfter,
+    breakBefore,
     aspectRatio,
 };
---
+++ src/styles/layout/breakBefore.ts
@@ -0,0 +1,26 @@
+export default {
+    'break-before-auto': {
+        breakBefore: 'auto',
+    },
+    'break-before-avoid': {
+        breakBefore: 'avoid',
+    },
+    'break-before-all': {
+        breakBefore: 'all',
+    },
+    'break-before-avoid-page': {
+        breakBefore: 'avoid-page',
+    },
+    'break-before-page': {
+        breakBefore: 'page',
+    },
+    'break-before-left': {
+        breakBefore: 'left',
+    },
+    'break-before-right': {
+        breakBefore: 'right',
+    },
+    'break-before-column': {
+        breakBefore: 'column',
+    },
+};
