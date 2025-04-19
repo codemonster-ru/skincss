86eacf498b26a29debd1d3fc4bd9b7c4865863c0
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-04-19 12:23:54 +0700

    Added styles of columns (layout).

--- src/styles/layout.ts
+++ src/styles/layout.ts
@@ -1,3 +1,4 @@
+import columns from './layout/columns';
 import aspectRatio from './layout/aspectRatio';
 
-export default { aspectRatio };
+export default { columns, aspectRatio };
---
+++ src/styles/layout/columns.ts
@@ -0,0 +1,44 @@
+export default {
+    'columns-3xs': {
+        columns: '16rem',
+    },
+    'columns-2xs': {
+        columns: '18rem',
+    },
+    'columns-xs': {
+        columns: '20rem',
+    },
+    'columns-sm': {
+        columns: '24rem',
+    },
+    'columns-md': {
+        columns: '28rem',
+    },
+    'columns-lg': {
+        columns: '32rem',
+    },
+    'columns-xl': {
+        columns: '36rem',
+    },
+    'columns-2xl': {
+        columns: '42rem',
+    },
+    'columns-3xl': {
+        columns: '48rem',
+    },
+    'columns-4xl': {
+        columns: '56rem',
+    },
+    'columns-5xl': {
+        columns: '64rem',
+    },
+    'columns-6xl': {
+        columns: '72rem',
+    },
+    'columns-7xl': {
+        columns: '80rem',
+    },
+    'columns-auto': {
+        columns: 'auto',
+    },
+};
