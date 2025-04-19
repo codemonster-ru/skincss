02841d90cf40f449d2cbd4fd5b7f9d38749cc908
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-04-19 12:47:28 +0700

    Added styles of break inside (layout).

--- src/styles/layout.ts
+++ src/styles/layout.ts
@@ -1,11 +1,13 @@
 import columns from './layout/columns';
 import breakAfter from './layout/breakAfter';
 import breakBefore from './layout/breakBefore';
+import breakInside from './layout/breakInside';
 import aspectRatio from './layout/aspectRatio';
 
 export default {
     columns,
     breakAfter,
     breakBefore,
+    breakInside,
     aspectRatio,
 };
---
+++ src/styles/layout/breakInside.ts
@@ -0,0 +1,14 @@
+export default {
+    'break-inside-auto': {
+        breakInside: 'auto',
+    },
+    'break-inside-avoid': {
+        breakInside: 'avoid',
+    },
+    'break-inside-avoid-page': {
+        breakInside: 'avoid-page',
+    },
+    'break-inside-avoid-column': {
+        breakInside: 'avoid-column',
+    },
+};
