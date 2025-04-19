2f1b76da885c6f38d4d319972327160d88cc7a76
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-04-19 12:32:03 +0700

    Added styles of break after (layout).

--- src/styles/layout.ts
+++ src/styles/layout.ts
@@ -1,4 +1,9 @@
 import columns from './layout/columns';
+import breakAfter from './layout/breakAfter';
 import aspectRatio from './layout/aspectRatio';
 
-export default { columns, aspectRatio };
+export default {
+    columns,
+    breakAfter,
+    aspectRatio,
+};
---
+++ src/styles/layout/breakAfter.ts
@@ -0,0 +1,26 @@
+export default {
+    'break-after-auto': {
+        breakAfter: 'auto',
+    },
+    'break-after-avoid': {
+        breakAfter: 'avoid',
+    },
+    'break-after-all': {
+        breakAfter: 'all',
+    },
+    'break-after-avoid-page': {
+        breakAfter: 'avoid-page',
+    },
+    'break-after-page': {
+        breakAfter: 'page',
+    },
+    'break-after-left': {
+        breakAfter: 'left',
+    },
+    'break-after-right': {
+        breakAfter: 'right',
+    },
+    'break-after-column': {
+        breakAfter: 'column',
+    },
+};
