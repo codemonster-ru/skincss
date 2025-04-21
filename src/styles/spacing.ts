845d6dbe73c385970cd961597a38b9de6aeed42d
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-04-21 23:08:27 +0700

    Added styles of padding (spacing).

--- src/styles/spacing.ts
+++ src/styles/spacing.ts
@@ -1,5 +1,7 @@
 import margin from './spacing/margin';
+import padding from './spacing/padding';
 
 export default {
     ...margin,
+    ...padding,
 };
---
+++ src/styles/spacing/padding.ts
@@ -0,0 +1,29 @@
+export default {
+    'p-px': {
+        padding: '1px',
+    },
+    'px-px': {
+        paddingInline: '1px',
+    },
+    'py-px': {
+        paddingBlock: '1px',
+    },
+    'ps-px': {
+        paddingInlineStart: '1px',
+    },
+    'pe-px': {
+        paddingInlineEnd: '1px',
+    },
+    'pt-px': {
+        paddingTop: '1px',
+    },
+    'pr-px': {
+        paddingRight: '1px',
+    },
+    'pb-px': {
+        paddingBottom: '1px',
+    },
+    'pl-px': {
+        paddingLeft: '1px',
+    },
+};
