f98a2fe6cc0fdcee219859c5c37a23da75b56f10
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-04-21 20:37:06 +0700

    Added styles of flex direction (flexBox and grid).

--- src/styles/grid.ts
+++ src/styles/grid.ts
@@ -1,5 +1,7 @@
 import flexBasis from './grid/flexBasis';
+import flexDirection from './grid/flexDirection';
 
 export default {
     ...flexBasis,
+    ...flexDirection,
 };
---
+++ src/styles/grid/flexDirection.ts
@@ -0,0 +1,14 @@
+export default {
+    'flex-row': {
+        flexDirection: 'row',
+    },
+    'flex-row-reverse': {
+        flexDirection: 'row-reverse',
+    },
+    'flex-col': {
+        flexDirection: 'column',
+    },
+    'flex-col-reverse': {
+        flexDirection: 'column-reverse',
+    },
+};
