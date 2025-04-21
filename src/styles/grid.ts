1ef3790c76a7082cc6cd92f169a57c0fc2f9a09f
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-04-21 21:51:07 +0700

    Added styles of align self (flexBox and grid).

--- src/styles/grid.ts
+++ src/styles/grid.ts
@@ -4,6 +4,7 @@ import gridRow from './grid/gridRow';
 import flexWrap from './grid/flexWrap';
 import flexGrow from './grid/flexGrow';
 import flexBasis from './grid/flexBasis';
+import alignSelf from './grid/alignSelf';
 import alignItems from './grid/alignItems';
 import flexShrink from './grid/flexShrink';
 import gridColumn from './grid/gridColumn';
@@ -26,6 +27,7 @@ export default {
     ...flexWrap,
     ...flexGrow,
     ...flexBasis,
+    ...alignSelf,
     ...alignItems,
     ...flexShrink,
     ...gridColumn,
---
+++ src/styles/grid/alignSelf.ts
@@ -0,0 +1,26 @@
+export default {
+    'self-auto': {
+        alignSelf: 'auto',
+    },
+    'self-start': {
+        alignSelf: 'flex-start',
+    },
+    'self-end': {
+        alignSelf: 'flex-end',
+    },
+    'self-end-safe': {
+        alignSelf: 'safe flex-end',
+    },
+    'self-center': {
+        alignSelf: 'center',
+    },
+    'self-center-safe': {
+        alignSelf: 'safe center',
+    },
+    'self-stretch': {
+        alignSelf: 'stretch',
+    },
+    'self-baseline': {
+        alignSelf: 'baseline',
+    },
+};
