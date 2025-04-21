0566bb98965e581e926fad3c6beafbffe21b550e
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-04-21 20:41:43 +0700

    Added styles of flex grow (flexBox and grid).

--- src/styles/grid.ts
+++ src/styles/grid.ts
@@ -1,11 +1,13 @@
 import flex from './grid/flex';
 import flexWrap from './grid/flexWrap';
+import flexGrow from './grid/flexGrow';
 import flexBasis from './grid/flexBasis';
 import flexDirection from './grid/flexDirection';
 
 export default {
     ...flex,
     ...flexWrap,
+    ...flexGrow,
     ...flexBasis,
     ...flexDirection,
 };
