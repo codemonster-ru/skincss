541675212652cc28e158670f9d83c070e4c2cbdf
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-04-21 20:40:09 +0700

    Added styles of flex (flexBox and grid).

--- src/styles/grid.ts
+++ src/styles/grid.ts
@@ -1,8 +1,10 @@
+import flex from './grid/flex';
 import flexWrap from './grid/flexWrap';
 import flexBasis from './grid/flexBasis';
 import flexDirection from './grid/flexDirection';
 
 export default {
+    ...flex,
     ...flexWrap,
     ...flexBasis,
     ...flexDirection,
---
+++ src/styles/grid/flex.ts
@@ -0,0 +1,11 @@
+export default {
+    'flex-auto': {
+        flex: '1 1 auto',
+    },
+    'flex-initial': {
+        flex: '0 1 auto',
+    },
+    'flex-none': {
+        flex: 'none',
+    },
+};
