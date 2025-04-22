62bf55c789d3e1b12f6486977a39774cf2c40135
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-04-22 22:28:18 +0700

    Added styles of min height (sizing).

--- src/styles/sizing.ts
+++ src/styles/sizing.ts
@@ -2,10 +2,12 @@ import width from './sizing/width';
 import height from './sizing/height';
 import minWidth from './sizing/minWidth';
 import maxWidth from './sizing/maxWidth';
+import minHeight from './sizing/minHeight';
 
 export default {
     ...width,
     ...height,
     ...minWidth,
     ...maxWidth,
+    ...minHeight,
 };
---
+++ src/styles/sizing/minHeight.ts
@@ -0,0 +1,41 @@
+export default {
+    'min-h-auto': {
+        minHeight: 'auto',
+    },
+    'min-h-px': {
+        minHeight: '1px',
+    },
+    'min-h-full': {
+        minHeight: '100%',
+    },
+    'min-h-screen': {
+        minHeight: '100vw',
+    },
+    'min-h-dvw': {
+        minHeight: '100dvw',
+    },
+    'min-h-dvh': {
+        minHeight: '100dvh',
+    },
+    'min-h-lvw': {
+        minHeight: '100lvw',
+    },
+    'min-h-lvh': {
+        minHeight: '100lvh',
+    },
+    'min-h-svw': {
+        minHeight: '100svw',
+    },
+    'min-h-svh': {
+        minHeight: '100svh',
+    },
+    'min-h-min': {
+        minHeight: 'min-content',
+    },
+    'min-h-max': {
+        minHeight: 'max-content',
+    },
+    'min-h-fit': {
+        minHeight: 'fit-content',
+    },
+};
