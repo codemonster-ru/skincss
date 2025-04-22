e4d8c54f2fd153949d285e27454557df6237dbe1
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-04-22 22:26:08 +0700

    Added styles of height (sizing).

--- src/styles/sizing.ts
+++ src/styles/sizing.ts
@@ -1,9 +1,11 @@
 import width from './sizing/width';
+import height from './sizing/height';
 import minWidth from './sizing/minWidth';
 import maxWidth from './sizing/maxWidth';
 
 export default {
     ...width,
+    ...height,
     ...minWidth,
     ...maxWidth,
 };
---
+++ src/styles/sizing/height.ts
@@ -0,0 +1,41 @@
+export default {
+    'h-auto': {
+        height: 'auto',
+    },
+    'h-px': {
+        height: '1px',
+    },
+    'h-full': {
+        height: '100%',
+    },
+    'h-screen': {
+        height: '100vw',
+    },
+    'h-dvw': {
+        height: '100dvw',
+    },
+    'h-dvh': {
+        height: '100dvh',
+    },
+    'h-lvw': {
+        height: '100lvw',
+    },
+    'h-lvh': {
+        height: '100lvh',
+    },
+    'h-svw': {
+        height: '100svw',
+    },
+    'h-svh': {
+        height: '100svh',
+    },
+    'h-min': {
+        height: 'min-content',
+    },
+    'h-max': {
+        height: 'max-content',
+    },
+    'h-fit': {
+        height: 'fit-content',
+    },
+};
