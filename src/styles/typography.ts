4468b7a948dc14d3f7d1e0b9db7df3dda56c1b45
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-04-30 00:19:52 +0700

    Added styles of font weight (typography).

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -2,10 +2,12 @@ import fontFamily from './typography/fontFamily';
 import fontSize from './typography/fontSize';
 import fontSmoothing from './typography/fontSmoothing';
 import fontStyle from './typography/fontStyle';
+import fontWeight from './typography/fontWeight';
 
 export default {
     ...fontFamily,
     ...fontSize,
     ...fontSmoothing,
     ...fontStyle,
+    ...fontWeight,
 };
