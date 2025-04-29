f6b18a6e22820428b5662278af47196b6559581f
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-04-29 23:55:28 +0700

    Added styles of font smoothing (typography).

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -1,7 +1,9 @@
 import fontFamily from './typography/fontFamily';
 import fontSize from './typography/fontSize';
+import fontSmoothing from './typography/fontSmoothing';
 
 export default {
     ...fontFamily,
     ...fontSize,
+    ...fontSmoothing,
 };
