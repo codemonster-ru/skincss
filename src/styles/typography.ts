0ce034fe60d6a8f33c9a2a2294c21744a653c3d9
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-04-30 23:43:12 +0700

    Added styles of font stretch (typography).

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -1,6 +1,7 @@
 import fontFamily from './typography/fontFamily';
 import fontSize from './typography/fontSize';
 import fontSmoothing from './typography/fontSmoothing';
+import fontStretch from './typography/fontStretch';
 import fontStyle from './typography/fontStyle';
 import fontWeight from './typography/fontWeight';
 
@@ -8,6 +9,7 @@ export default {
     ...fontFamily,
     ...fontSize,
     ...fontSmoothing,
+    ...fontStretch,
     ...fontStyle,
     ...fontWeight,
 };
---
+++ src/styles/typography/fontStretch.ts
@@ -0,0 +1,29 @@
+export default {
+    'font-stretch-ultra-condensed': {
+        fontStretch: 'ultra-condensed',
+    },
+    'font-stretch-extra-condensed': {
+        fontStretch: 'extra-condensed',
+    },
+    'font-stretch-condensed': {
+        fontStretch: 'condensed',
+    },
+    'font-stretch-semi-condensed': {
+        fontStretch: 'semi-condensed',
+    },
+    'font-stretch-normal': {
+        fontStretch: 'normal',
+    },
+    'font-stretch-semi-expanded': {
+        fontStretch: 'semi-expanded',
+    },
+    'font-stretch-expanded': {
+        fontStretch: 'expanded',
+    },
+    'font-stretch-extra-expanded': {
+        fontStretch: 'extra-expanded',
+    },
+    'font-stretch-ultra-expanded': {
+        fontStretch: 'ultra-expanded',
+    },
+};
