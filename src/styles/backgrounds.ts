efac1bca142aa78acfe1e521a8d4425ff23f895e
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-05-01 14:45:13 +0700

    Added styles of background clip (typography).

--- src/styles/backgrounds.ts
+++ src/styles/backgrounds.ts
@@ -1,5 +1,7 @@
 import backgroundAttachment from './backgrounds/backgroundAttachment';
+import backgroundClip from './backgrounds/backgroundClip';
 
 export default {
     ...backgroundAttachment,
+    ...backgroundClip,
 };
---
+++ src/styles/backgrounds/backgroundClip.ts
@@ -0,0 +1,14 @@
+export default {
+    'bg-clip-border': {
+        backgroundClip: 'border-box',
+    },
+    'bg-clip-padding': {
+        backgroundClip: 'padding-box',
+    },
+    'bg-clip-content': {
+        backgroundClip: 'content-box',
+    },
+    'bg-clip-text': {
+        backgroundClip: 'text',
+    },
+};
