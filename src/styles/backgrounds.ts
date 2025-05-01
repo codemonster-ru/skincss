c7c444a1fcf0243fb0dab21ffcfb726aaca34694
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-05-01 18:22:37 +0700

    Added styles of background origin (typography).

--- src/styles/backgrounds.ts
+++ src/styles/backgrounds.ts
@@ -2,10 +2,12 @@ import backgroundAttachment from './backgrounds/backgroundAttachment';
 import backgroundClip from './backgrounds/backgroundClip';
 import backgroundColor from './backgrounds/backgroundColor';
 import backgroundImage from './backgrounds/backgroundImage';
+import backgroundOrigin from './backgrounds/backgroundOrigin';
 
 export default {
     ...backgroundAttachment,
     ...backgroundClip,
     ...backgroundColor,
     ...backgroundImage,
+    ...backgroundOrigin,
 };
---
+++ src/styles/backgrounds/backgroundOrigin.ts
@@ -0,0 +1,11 @@
+export default {
+    'bg-origin-border': {
+        backgroundOrigin: 'border-box',
+    },
+    'bg-origin-padding': {
+        backgroundOrigin: 'padding-box',
+    },
+    'bg-origin-content': {
+        backgroundOrigin: 'content-box',
+    },
+};
