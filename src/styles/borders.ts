0ce2a0e059cc246a11c5bc8102f3891943300fb8
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-05-01 21:18:25 +0700

    Added styles of border style (borders).

--- src/styles/borders.ts
+++ src/styles/borders.ts
@@ -1,9 +1,11 @@
 import borderColor from './borders/borderColor';
 import borderRadius from './borders/borderRadius';
+import borderStyle from './borders/borderStyle';
 import borderWidth from './borders/borderWidth';
 
 export default {
     ...borderColor,
     ...borderRadius,
+    ...borderStyle,
     ...borderWidth,
 };
---
+++ src/styles/borders/borderStyle.ts
@@ -0,0 +1,20 @@
+export default {
+    'border-solid': {
+        borderStyle: 'solid',
+    },
+    'border-dashed': {
+        borderStyle: 'dashed',
+    },
+    'border-dotted': {
+        borderStyle: 'dotted',
+    },
+    'border-double': {
+        borderStyle: 'double',
+    },
+    'border-hidden': {
+        borderStyle: 'hidden',
+    },
+    'border-none': {
+        borderStyle: 'none',
+    },
+};
