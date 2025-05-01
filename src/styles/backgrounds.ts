5e861beb4eabceb65cd6339cbcb65af4992a74ef
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-05-01 18:21:27 +0700

    Added styles of background image (typography).

--- src/styles/backgrounds.ts
+++ src/styles/backgrounds.ts
@@ -1,9 +1,11 @@
 import backgroundAttachment from './backgrounds/backgroundAttachment';
 import backgroundClip from './backgrounds/backgroundClip';
 import backgroundColor from './backgrounds/backgroundColor';
+import backgroundImage from './backgrounds/backgroundImage';
 
 export default {
     ...backgroundAttachment,
     ...backgroundClip,
     ...backgroundColor,
+    ...backgroundImage,
 };
---
+++ src/styles/backgrounds/backgroundImage.ts
@@ -0,0 +1,32 @@
+export default {
+    'bg-none': {
+        backgroundImage: 'none',
+    },
+    'bg-linear-to-t': {
+        backgroundImage: 'linear-gradient(to top, var(--gradient-stops))',
+    },
+    'bg-linear-to-tr': {
+        backgroundImage: 'linear-gradient(to top right, var(--gradient-stops))',
+    },
+    'bg-linear-to-r': {
+        backgroundImage: 'linear-gradient(to right, var(--gradient-stops))',
+    },
+    'bg-linear-to-br': {
+        backgroundImage: 'linear-gradient(to bottom right, var(--gradient-stops))',
+    },
+    'bg-linear-to-b': {
+        backgroundImage: 'linear-gradient(to bottom, var(--gradient-stops))',
+    },
+    'bg-linear-to-bl': {
+        backgroundImage: 'linear-gradient(to bottom left, var(--gradient-stops))',
+    },
+    'bg-linear-to-l': {
+        backgroundImage: 'linear-gradient(to left, var(--gradient-stops))',
+    },
+    'bg-linear-to-tl': {
+        backgroundImage: 'linear-gradient(to top left, var(--gradient-stops))',
+    },
+    'bg-radial': {
+        backgroundImage: 'radial-gradient(in oklab, var(--gradient-stops))',
+    },
+};
