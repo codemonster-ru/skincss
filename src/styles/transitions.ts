daa2f31652648252560c0e7ccbe242185785fa89
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-05-03 19:35:50 +0700

    Added styles of transition timing function (transitions & animation).

--- src/styles/transitions.ts
+++ src/styles/transitions.ts
@@ -1,9 +1,11 @@
 import transitionBehavior from './transitions/transitionBehavior';
 import transitionDuration from './transitions/transitionDuration';
 import transitionProperty from './transitions/transitionProperty';
+import transitionTimingFunction from './transitions/transitionTimingFunction';
 
 export default {
     ...transitionBehavior,
     ...transitionDuration,
     ...transitionProperty,
+    ...transitionTimingFunction,
 };
---
+++ src/styles/transitions/transitionTimingFunction.ts
@@ -0,0 +1,17 @@
+export default {
+    'ease-linear': {
+        transitionTimingFunction: 'linear',
+    },
+    'ease-in': {
+        transitionTimingFunction: 'var(--ease-in)',
+    },
+    'ease-out': {
+        transitionTimingFunction: 'var(--ease-out)',
+    },
+    'ease-in-out': {
+        transitionTimingFunction: 'var(--ease-in-out)',
+    },
+    'ease-initial': {
+        transitionTimingFunction: 'initial',
+    },
+};
