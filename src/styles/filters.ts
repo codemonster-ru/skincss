ee415489bd5b3a5682148643c3a175f27ea48562
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-05-03 17:14:11 +0700

    Added styles of drop shadow (filters / filter).

--- src/styles/filters.ts
+++ src/styles/filters.ts
@@ -1,9 +1,11 @@
 import blur from './filters/filter/blur';
 import brightness from './filters/filter/brightness';
 import contrast from './filters/filter/contrast';
+import dropShadow from './filters/filter/dropShadow';
 
 export default {
     ...blur,
     ...brightness,
     ...contrast,
+    ...dropShadow,
 };
