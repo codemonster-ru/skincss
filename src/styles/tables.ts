e1e0072ef30be08228df1e75bc9d3b19d94ff1fd
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-05-03 17:52:35 +0700

    Added styles of border spacing (tables).

--- src/styles/tables.ts
+++ src/styles/tables.ts
@@ -1,5 +1,7 @@
 import borderCollapse from './tables/borderCollapse';
+import borderSpacing from './tables/borderSpacing';
 
 export default {
     ...borderCollapse,
+    ...borderSpacing,
 };
---
+++ src/styles/tables/borderSpacing.ts
@@ -0,0 +1,5 @@
+export default {
+    'border-spacing-<number>': {
+        borderSpacing: 'calc(var(--spacing) * <number>)',
+    },
+};
