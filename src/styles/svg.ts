d4f65b3876713d0cc6338e27f6bef5a0cdd2af23
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-05-03 22:29:08 +0700

    Added styles of stroke width (svg).

--- src/styles/svg.ts
+++ src/styles/svg.ts
@@ -1,7 +1,9 @@
 import fill from './svg/fill';
 import stroke from './svg/stroke';
+import strokeWidth from './svg/strokeWidth';
 
 export default {
     ...fill,
     ...stroke,
+    ...strokeWidth,
 };
---
+++ src/styles/svg/strokeWidth.ts
@@ -0,0 +1,5 @@
+export default {
+    // 'stroke-<number>': {
+    //     strokeWidth: '<number>',
+    // },
+};
