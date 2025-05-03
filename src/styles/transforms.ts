8616ecfc52fe506caaf345d4abc75980c2fdb889
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-05-03 20:58:24 +0700

    Added styles of perspective origin (transforms).

--- src/styles/transforms.ts
+++ src/styles/transforms.ts
@@ -1,7 +1,9 @@
 import backfaceVisibility from './transforms/backfaceVisibility';
 import perspective from './transforms/perspective';
+import perspectiveOrigin from './transforms/perspectiveOrigin';
 
 export default {
     ...backfaceVisibility,
     ...perspective,
+    ...perspectiveOrigin,
 };
---
+++ src/styles/transforms/perspectiveOrigin.ts
@@ -0,0 +1,29 @@
+export default {
+    'perspective-origin-center': {
+        perspectiveOrigin: 'center',
+    },
+    'perspective-origin-top': {
+        perspectiveOrigin: 'top',
+    },
+    'perspective-origin-top-right': {
+        perspectiveOrigin: 'top right',
+    },
+    'perspective-origin-right': {
+        perspectiveOrigin: 'right',
+    },
+    'perspective-origin-bottom-right': {
+        perspectiveOrigin: 'bottom right',
+    },
+    'perspective-origin-bottom': {
+        perspectiveOrigin: 'bottom',
+    },
+    'perspective-origin-bottom-left': {
+        perspectiveOrigin: 'bottom left',
+    },
+    'perspective-origin-left': {
+        perspectiveOrigin: 'left',
+    },
+    'perspective-origin-top-left': {
+        perspectiveOrigin: 'top left',
+    },
+};
