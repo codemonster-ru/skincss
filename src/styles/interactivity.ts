59c59e6fe641b70cfa123bd45fb99dee70a675c2
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-05-03 21:39:21 +0700

    Added styles of color scheme (interactivity).

--- src/styles/interactivity.ts
+++ src/styles/interactivity.ts
@@ -1,9 +1,11 @@
 import accentColor from './interactivity/accentColor';
 import appearance from './interactivity/appearance';
 import caretColor from './interactivity/caretColor';
+import colorScheme from './interactivity/colorScheme';
 
 export default {
     ...accentColor,
     ...appearance,
     ...caretColor,
+    ...colorScheme,
 };
---
+++ src/styles/interactivity/colorScheme.ts
@@ -0,0 +1,20 @@
+export default {
+    'scheme-normal': {
+        colorScheme: 'normal',
+    },
+    'scheme-dark': {
+        colorScheme: 'dark',
+    },
+    'scheme-light': {
+        colorScheme: 'light',
+    },
+    'scheme-light-dark': {
+        colorScheme: 'light dark',
+    },
+    'scheme-only-dark': {
+        colorScheme: 'only dark',
+    },
+    'scheme-only-light': {
+        colorScheme: 'only light',
+    },
+};
