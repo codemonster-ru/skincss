68e982396994940dcd064f35f853680957a48c3c
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-05-03 21:46:50 +0700

    Added styles of cursor (interactivity).

--- src/styles/interactivity.ts
+++ src/styles/interactivity.ts
@@ -2,10 +2,12 @@ import accentColor from './interactivity/accentColor';
 import appearance from './interactivity/appearance';
 import caretColor from './interactivity/caretColor';
 import colorScheme from './interactivity/colorScheme';
+import cursor from './interactivity/cursor';
 
 export default {
     ...accentColor,
     ...appearance,
     ...caretColor,
     ...colorScheme,
+    ...cursor,
 };
