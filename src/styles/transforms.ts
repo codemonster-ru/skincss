4b2a958fa48c0b9ee2d1db0310bfec772dca57be
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-05-03 21:03:52 +0700

    Added styles of scale (transforms).

--- src/styles/transforms.ts
+++ src/styles/transforms.ts
@@ -2,10 +2,12 @@ import backfaceVisibility from './transforms/backfaceVisibility';
 import perspective from './transforms/perspective';
 import perspectiveOrigin from './transforms/perspectiveOrigin';
 import rotate from './transforms/rotate';
+import scale from './transforms/scale';
 
 export default {
     ...backfaceVisibility,
     ...perspective,
     ...perspectiveOrigin,
     ...rotate,
+    ...scale,
 };
---
+++ src/styles/transforms/scale.ts
@@ -0,0 +1,8 @@
+export default {
+    'scale-none': {
+        scale: 'none',
+    },
+    'scale-3d': {
+        scale: 'var(--scale-x) var(--scale-y) var(--scale-z)',
+    },
+};
