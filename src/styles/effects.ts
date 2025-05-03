637d7166aa51175b509500d404f15db72a178e42
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-05-03 15:30:03 +0700

    Added styles of mix blend mode (effects).

--- src/styles/effects.ts
+++ src/styles/effects.ts
@@ -1,9 +1,11 @@
 import boxShadow from './effects/boxShadow';
+import mixBlendMode from './effects/mixBlendMode';
 import opacity from './effects/opacity';
 import textShadow from './effects/textShadow';
 
 export default {
     ...boxShadow,
+    ...mixBlendMode,
     ...opacity,
     ...textShadow,
 };
---
+++ src/styles/effects/mixBlendMode.ts
@@ -0,0 +1,56 @@
+export default {
+    'mix-blend-normal': {
+        mixBlendMode: 'normal',
+    },
+    'mix-blend-multiply': {
+        mixBlendMode: 'multiply',
+    },
+    'mix-blend-screen': {
+        mixBlendMode: 'screen',
+    },
+    'mix-blend-overlay': {
+        mixBlendMode: 'overlay',
+    },
+    'mix-blend-darken': {
+        mixBlendMode: 'darken',
+    },
+    'mix-blend-lighten': {
+        mixBlendMode: 'lighten',
+    },
+    'mix-blend-color-dodge': {
+        mixBlendMode: 'color-dodge',
+    },
+    'mix-blend-color-burn': {
+        mixBlendMode: 'color-burn',
+    },
+    'mix-blend-hard-light': {
+        mixBlendMode: 'hard-light',
+    },
+    'mix-blend-soft-light': {
+        mixBlendMode: 'soft-light',
+    },
+    'mix-blend-difference': {
+        mixBlendMode: 'difference',
+    },
+    'mix-blend-exclusion': {
+        mixBlendMode: 'exclusion',
+    },
+    'mix-blend-hue': {
+        mixBlendMode: 'hue',
+    },
+    'mix-blend-saturation': {
+        mixBlendMode: 'saturation',
+    },
+    'mix-blend-color': {
+        mixBlendMode: 'color',
+    },
+    'mix-blend-luminosity': {
+        mixBlendMode: 'luminosity',
+    },
+    'mix-blend-plus-darker': {
+        mixBlendMode: 'plus-darker',
+    },
+    'mix-blend-plus-lighter': {
+        mixBlendMode: 'plus-lighter',
+    },
+};
