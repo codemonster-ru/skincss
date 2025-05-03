c32f7727142b14cfd66e7ba2514526907730a85c
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-05-03 15:34:57 +0700

    Added styles of background blend mode (effects).

--- src/styles/effects.ts
+++ src/styles/effects.ts
@@ -1,9 +1,11 @@
+import backgroundBlendMode from './effects/backgroundBlendMode';
 import boxShadow from './effects/boxShadow';
 import mixBlendMode from './effects/mixBlendMode';
 import opacity from './effects/opacity';
 import textShadow from './effects/textShadow';
 
 export default {
+    ...backgroundBlendMode,
     ...boxShadow,
     ...mixBlendMode,
     ...opacity,
---
+++ src/styles/effects/backgroundBlendMode.ts
@@ -0,0 +1,50 @@
+export default {
+    'bg-blend-normal': {
+        backgroundBlendMode: 'normal',
+    },
+    'bg-blend-multiply': {
+        backgroundBlendMode: 'multiply',
+    },
+    'bg-blend-screen': {
+        backgroundBlendMode: 'screen',
+    },
+    'bg-blend-overlay': {
+        backgroundBlendMode: 'overlay',
+    },
+    'bg-blend-darken': {
+        backgroundBlendMode: 'darken',
+    },
+    'bg-blend-lighten': {
+        backgroundBlendMode: 'lighten',
+    },
+    'bg-blend-color-dodge': {
+        backgroundBlendMode: 'color-dodge',
+    },
+    'bg-blend-color-burn': {
+        backgroundBlendMode: 'color-burn',
+    },
+    'bg-blend-hard-light': {
+        backgroundBlendMode: 'hard-light',
+    },
+    'bg-blend-soft-light': {
+        backgroundBlendMode: 'soft-light',
+    },
+    'bg-blend-difference': {
+        backgroundBlendMode: 'difference',
+    },
+    'bg-blend-exclusion': {
+        backgroundBlendMode: 'exclusion',
+    },
+    'bg-blend-hue': {
+        backgroundBlendMode: 'hue',
+    },
+    'bg-blend-saturation': {
+        backgroundBlendMode: 'saturation',
+    },
+    'bg-blend-color': {
+        backgroundBlendMode: 'color',
+    },
+    'bg-blend-luminosity': {
+        backgroundBlendMode: 'luminosity',
+    },
+};
