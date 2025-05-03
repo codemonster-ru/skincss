493d74e276de2770548f82923092f89ceee859d8
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-05-03 17:55:03 +0700

    Added styles of caption side (tables).

--- src/styles/tables.ts
+++ src/styles/tables.ts
@@ -1,9 +1,11 @@
 import borderCollapse from './tables/borderCollapse';
 import borderSpacing from './tables/borderSpacing';
+import captionSide from './tables/captionSide';
 import tableLayout from './tables/tableLayout';
 
 export default {
     ...borderCollapse,
     ...borderSpacing,
+    ...captionSide,
     ...tableLayout,
 };
---
+++ src/styles/tables/captionSide.ts
@@ -0,0 +1,8 @@
+export default {
+    'caption-top': {
+        captionSide: 'top',
+    },
+    'caption-bottom': {
+        captionSide: 'bottom',
+    },
+};
