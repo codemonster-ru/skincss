1943d7b3e311c973b9b56ee4c64b0098ae3c434d
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-05-03 15:14:34 +0700

    Added styles of opacity (effects).

--- src/styles/effects.ts
+++ src/styles/effects.ts
@@ -1,7 +1,9 @@
 import boxShadow from './effects/boxShadow';
+import opacity from './effects/opacity';
 import textShadow from './effects/textShadow';
 
 export default {
     ...boxShadow,
+    ...opacity,
     ...textShadow,
 };
---
+++ src/styles/effects/opacity.ts
@@ -0,0 +1,5 @@
+export default {
+    // 'opacity-<number>': {
+    //     opacity: '<number>%',
+    // },
+};
