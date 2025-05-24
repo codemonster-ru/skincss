7189fd8cc98fc73658f9d9920c960954c063e669
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-05-24 19:11:32 +0700

    Added export of all styles in one file.

---
+++ src/styles/index.ts
@@ -0,0 +1,35 @@
+import accessibility from './accessibility';
+import backgrounds from './backgrounds';
+import base from './base';
+import borders from './borders';
+import effects from './effects';
+import filters from './filters';
+import grid from './grid';
+import interactivity from './interactivity';
+import layout from './layout';
+import sizing from './sizing';
+import spacing from './spacing';
+import svg from './svg';
+import tables from './tables';
+import transforms from './transforms';
+import transitions from './transitions';
+import typography from './typography';
+
+export default {
+    ...accessibility,
+    ...backgrounds,
+    ...base,
+    ...borders,
+    ...effects,
+    ...filters,
+    ...grid,
+    ...interactivity,
+    ...layout,
+    ...sizing,
+    ...spacing,
+    ...svg,
+    ...tables,
+    ...transforms,
+    ...transitions,
+    ...typography,
+};
