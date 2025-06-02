798236e419f92141ca7b6a8d6880e8d1d63a66d1
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-06-02 22:53:50 +0700

    added arbitrary styles to tables.

--- src/styles/tables/borderSpacing.ts
+++ src/styles/tables/borderSpacing.ts
@@ -1,5 +1,31 @@
 export default {
-    // 'border-spacing-<number>': {
-    //     borderSpacing: 'calc(var(--spacing) * <number>)',
-    // },
+    arbitraryValues: {
+        'border-spacing-<number>': {
+            borderSpacing: 'calc(var(--spacing) * <number>)',
+        },
+        'border-spacing-(<variable>)': {
+            borderSpacing: 'var(<variable>)',
+        },
+        'border-spacing-[<value>]': {
+            borderSpacing: '<value>',
+        },
+        'border-spacing-x-<number>': {
+            borderSpacing: 'calc(var(--spacing) * <number>) var(--border-spacing-y)',
+        },
+        'border-spacing-x-(<variable>)': {
+            borderSpacing: 'var(<variable>) var(--border-spacing-y)',
+        },
+        'border-spacing-x-[<value>]': {
+            borderSpacing: '<value> var(--border-spacing-y)',
+        },
+        'border-spacing-y-<number>': {
+            borderSpacing: 'var(--border-spacing-x) calc(var(--spacing) * <number>)',
+        },
+        'border-spacing-y-(<variable>)': {
+            borderSpacing: 'var(--border-spacing-x) var(<variable>)',
+        },
+        'border-spacing-y-[<value>]': {
+            borderSpacing: 'var(--border-spacing-x) <value>',
+        },
+    },
 };
