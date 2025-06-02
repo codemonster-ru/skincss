bd99ca60e733a8afd4ef7c78ed68cebd373b618b
Kirill Kolesnikov <kolesnikov-kiril@mail.ru>
2025-06-02 22:52:52 +0700

    Added styles of gap (grid).

---
+++ src/styles/grid/gap.ts
@@ -0,0 +1,31 @@
+export default {
+    arbitraryValues: {
+        'gap-<number>': {
+            gap: 'calc(var(--spacing) * <value>)',
+        },
+        'gap-(<variable>)': {
+            gap: 'var(<variable>)',
+        },
+        'gap-[<value>]': {
+            gap: '<value>',
+        },
+        'gap-x-<number>': {
+            columnGap: 'calc(var(--spacing) * <value>)',
+        },
+        'gap-x-(<variable>)': {
+            columnGap: 'var(<variable>)',
+        },
+        'gap-x-[<value>]': {
+            columnGap: '<value>',
+        },
+        'gap-y-<number>': {
+            rowGap: 'calc(var(--spacing) * <value>)',
+        },
+        'gap-y-(<variable>)': {
+            rowGap: 'var(<variable>)',
+        },
+        'gap-y-[<value>]': {
+            rowGap: '<value>',
+        },
+    },
+};
